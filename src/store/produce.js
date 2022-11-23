03b433dc8bf79dea563fe323b7ff3954ac4908fb
Arnob <arnobdam@gmail.com>
2022-11-23 14:36:04 -0500

    completed produce reducer

--- src/store/produce.js
+++ src/store/produce.js
@@ -1,8 +1,29 @@
-
+import produceData from '../mockData/produce.json'
 
 export default function produceReducer(state = {}, action) {
+    Object.freeze(state)
+
+    // const nextState = {...state}
+
+
+
     switch (action.type) {
+        case POPULATE:
+            const produceObject = {}
+            action.produce.forEach(item => (
+                produceObject[item.id] = item
+            ))
+            return produceObject;
         default:
             return state;
+    }
+}
+
+const POPULATE = "produce/POPULATE"
+
+export const populateProduce = () => {
+    return {
+        type: POPULATE,
+        produce: produceData
     }
 }
